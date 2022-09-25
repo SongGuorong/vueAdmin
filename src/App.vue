ae6b5e491108506f7b0df3bfe344357d9c847a3e
sgr0216 <grs59680@gmail.com>
2022-09-25 23:01:41 +0800

    add element plus

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,7 @@ import Count from "./components/Count.vue";
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-    </div>
+    <!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
   </header>
 
   <main>
--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,22 +1,21 @@
 <template>
-  <button type="button" @click="count++"> {{count}} </button>
+  <!-- 视图(view) -->
+  <el-row style="margin: 25px 0 0 25px;">
+    <el-button type="primary" @click="increment">{{ count }}</el-button>
+  </el-row>
 </template>
 
-<script>
-import {ref} from "vue";
+<!--单文件组件script setup-->
+<script setup>
+import {ref, onMounted} from "vue";
 
-export default {
-  name: "Count",
-  setup() {
-    const count = ref(0);
-    return {
-      count,
-    };
-  },
-  mounted() {
-    console.log(this.count);
-  },
-};
+// 状态(state)
+const count = ref(0);
+// 动作(actions)
+const increment = () => count.value++;
+// 生命周期钩子函数
+onMounted(() => console.log("hello vue3"));
+
 </script>
 
 <style scoped></style>
