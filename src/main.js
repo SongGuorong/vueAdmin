ca74e62e74eb640140b1d790d727a472cf463774
SongGuorong <guorong.song@bl.com>
2022-10-06 22:20:51 +0800

    构建LoginForm

--- src/main.js
+++ src/main.js
@@ -1,64 +1,15 @@
-import {createApp} from 'vue'
+import {createApp} from 'vue';
 import { createStore } from 'vuex'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-// import './assets/main.css'
 
-// 创建一个新的store实例
-const store = createStore({
-    state: {
-        count: 0
-    },
-    // 当前状态即刻变化，属于同步事务
-    mutations: {
-        increment (state) {
-            state.count++
-        }
-    },
-    actions: {
-        // context上下文对象
-        increment (context) {
-            context.commit('increment')
-        },
-        incrementAsync (context) {
-            setTimeout(() => {
-                context.commit('increment')
-            }, 1000)
-        }
-    }
-});
 
-const store2 = createStore({
-    state: {
-        todos: [
-            {id:1, text:'已完成', done: true},
-            {id:2, text:'未完成', done: false}
-        ]
-    },
-    getters: {
-        doneTodos (state) {
-            return state.todos.filter(todo => todo.done)
-        }
-    }
-})
 
 // 根组件
 const app = createApp(App)
 
 app.use(ElementPlus)
 
-app.use(store)
-app.use(store2)
-
-console.log(store.state.count)
-
-// store.commit('increment')
-
-store.dispatch('incrementAsync').then(() => console.log("接下来: "+ store.state.count))
-
-// store.dispatch('increment').then(() => console.log("接下来: "+ store.state.count))
-
-console.log(store.state.count)
 app.mount('#app')
---
+++ src/views/login/comp/LoginForm.vue
@@ -0,0 +1,137 @@
+<template>
+  <el-form :model="ruleForm" :rules="rules" ref="validateForm" class="login-ruleForm">
+    <el-form-item prop="username">
+      <el-input :placeholder="t('login.username')" v-model="ruleForm.username">
+        <template>
+          <user theme="outline" size="16" fill="#999"/>
+        </template>
+      </el-input>
+    </el-form-item>
+
+    <el-form-item prop="password">
+      <el-input :placeholder="t('login.password')" type="password" v-model="ruleForm.password" @keyup.enter="handleLogin">
+        <template>
+          <lock theme="outline" size="16" fill="#999"/>
+        </template>
+      </el-input>
+    </el-form-item>
+
+    <el-form-item>
+      <div class="login-check">
+        <el-checkbox v-model="checkedPwd">{{t('login.rememberPwd')}}</el-checkbox>
+        <el-button text type="primary">{{t('login.forgotPwd')}}</el-button>
+      </div>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="primary" :loading="loading" class="login-btn" round @click="handleLogin">
+        {{t('login.loginBtn')}}
+      </el-button>
+    </el-form-item>
+
+    <el-divider>{{t('login.thirdparty')}}</el-divider>
+
+    <el-form-item>
+      <div class="login-methods">
+        <wechat theme="outline" size="24" fill="#333" />
+        <alipay theme="outline" size="24" fill="#333" />
+        <github theme="outline" size="24" fill="#333" />
+        <twitter theme="outline" size="24" fill="#333" />
+        <google theme="outline" size="24" fill="#333" />
+      </div>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+import {useI18n} from "vue-i18n";
+import {useStore} from "vuex";
+import {useRouter} from "vue-router";
+import {reactive, ref, toRefs, unref, watch} from "vue";
+
+export default {
+  name: "LoginForm",
+  setup() {
+    const {t} = useI18n();
+    const store = useStore();
+    const router = useRouter();
+    const validateForm = ref(null);
+    const state = reactive({
+      ruleForm: {
+        username: 'admin',
+        password: 'admin'
+      },
+      loading: false,
+      checkedPwd: false,
+      redirect: undefined,
+      rules: {  /*校验规则, trigger: 'blur'失焦时触发*/
+        username: [{required: true, message: t('login.rules.username'), trigger: 'blur'}],
+        password: [{required: true, message: t('login.rules.password'), trigger: 'blur'}],
+      }
+    });
+
+    // 监听
+    watch(
+        () => router.currentRoute,
+        ({_value}) => {   /*_value等价于router.currentRoute*/
+          const route = _value;
+          state.redirect = (route.query && route.query.redirect) || '/';
+        },
+        {
+          immediate: true
+        }
+    );
+
+    // 登录
+    const handleLogin = async () => {
+      const form = unref(validateForm); // unref()：是 val = isRef(val) ? val.value : val 的语法糖。
+      if (!form) return;
+      await form.validate((valid) => {
+        if (valid) {
+          state.loading = true;
+          store.dispatch('user/login', state.ruleForm)
+              .then(() => {
+                const routerPath = (state.redirect === '/404' || state.redirect === '/401')? '/' : state.redirect;
+                router.push(routerPath).catch(() => {});
+                state.loading = false;
+              })
+              .catch(() => {
+                state.loading = false;
+              });
+        }
+      });
+    };
+    return {
+      ...toRefs(state), // ...解构出来不丢失响应性
+      validateForm,
+      handleLogin,
+      t,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .login-ruleForm {
+    margin-top: 1rem;
+    :deep(.el-input__prefix) {
+      top: 2px;
+      padding: 0 4px;
+    }
+    .login-methods {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-around; /*space-around 每个item 左右方向的margin相等。两个item中间的间距会比较大*/
+    }
+    .login-btn {
+      width: 100%;
+    }
+    .login-check {
+      width: 100%;
+      display: flex;
+      align-content: center;
+      justify-content: space-between; /*space-between 最左、最右item贴合左侧或右侧边框，item与item之间间距相等。*/
+    }
+  }
+</style>
